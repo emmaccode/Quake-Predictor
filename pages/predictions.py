1f04d015ad14b768772e139f848a803d2cd7b710
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2019-09-27 15:31:14 -0400

    Add files via upload

--- pages/predictions.py
+++ pages/predictions.py
@@ -2,10 +2,10 @@ import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
+import scikit-learn
 from dash.dependencies import Input, Output
 
 from app import app
-import scikit-learn as sklearn
 column1 = dbc.Col(
     [
         dcc.Markdown(
