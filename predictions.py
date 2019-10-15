3b37f0cccb40b2d57328856f5cf6e6bdc44bea56
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2019-10-15 01:00:53 -0400

    Add files via upload

--- predictions.py
+++ predictions.py
@@ -2,7 +2,7 @@ import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
-import scikit-learn as sklearn
+import sklearn
 from dash.dependencies import Input, Output
 
 from app import app
